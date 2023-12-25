ea90ed0c1424a5bd1b9bb14ad6cb0d00c3f9fe72
AndrewNikitin127 <donandrain153@gmail.com>
2023-12-25 18:49:59 +0500

    second task complete

---
+++ src/ArraySchema.js
@@ -0,0 +1,16 @@
+export default class ArraySchema {
+  validators = [(val) => Array.isArray(val)];
+
+  constructor (newValidators){
+    if(newValidators) this.validators = newValidators;
+  }
+
+  isValid(val) {
+    return this.validators.every( validator => validator(val));
+  }
+
+  allIntegers(){
+    const validator = (array) => array.every( element => Number.isInteger(element));
+    return new ArraySchema([...this.validators, validator]); 
+  }
+}
